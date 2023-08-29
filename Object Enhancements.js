511e340dceac4235df65b468703cd954a800d75e
Patrick <pshinab@yahoo.comm>
2023-08-29 07:50:48 -0700

    Object Enhancements

---
+++ Object Enhancements.js
@@ -0,0 +1,57 @@
+function createInstructor(firstName, lastName){
+  return {
+    firstName,
+    lastName
+  }
+}
+
+let favoriteNumber = 42;
+
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+}
+
+const instructor = {
+  firstName: "Colt",
+  sayHi(){
+    return "Hi!";
+  },
+  sayBye(){
+    return this.firstName + " says bye!";
+  }
+}
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise){
+  return {
+    species,
+    [verb](){
+      return noise;
+    }
+  }
+}
+
+const d = createAnimal("dog", "bark", "Woooof!")
+d.bark()  
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+s.bleet() 
+
+function createAnimal(species, verb, noise){
+  return {
+    species,
+    [verb](){
+      return noise;
+    }
+  }
+}
+
+
